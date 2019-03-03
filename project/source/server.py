577a6c985b6a5789adaa84769de0bf83c2fb394b
Eric Bower <epbower@iu.edu>
2019-03-03 17:56:15 -0500

    Added some web code

---
+++ project/source/server.py
@@ -0,0 +1,24 @@
+"""
+Main module of the server file
+"""
+
+from flask import render_template
+import connexion
+
+
+# Create the application instance
+app = connexion.App(__name__, specification_dir="./")
+
+# Read the yaml file to configure the endpoints
+app.add_api("master.yaml")
+
+# create a URL route in our application for "/"
+@app.route("/")
+def home():
+   # msg = {"msg": "It's working!"}
+   # return jsonify(msg)
+   return render_template("home.html")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
