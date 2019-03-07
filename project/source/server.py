56ccff8c8248546f60ceab70ac44dd722dee368a
Eric Bower <ebower42@gmail.com>
2019-03-07 17:32:36 -0500

    Update

--- project/source/server.py
+++ project/source/server.py
@@ -16,6 +16,9 @@ app.add_api("master.yml")
 @app.route("/")
 def home():
    return render_template("home.html")
+@app.route("/templates/other.html")
+def other():
+   return render_template("other.html")
 
 
 if __name__ == "__main__":
