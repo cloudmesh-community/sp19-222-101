59a11ec4912a7d4926324e7ddc779059b2f6ad2b
Eric Bower <ebower42@gmail.com>
2019-04-03 22:30:10 -0400

    Updated model and data

--- project/project-code/py_scripts/model.py
+++
@@ -1,5 +0,0 @@
-from flask import render_template
-def showModel():
-    
-    tst = "HELLO WORLD!"
-    return render_template("showModel.html", prediction="Spam")
--- project/project-code/server.py
+++ project/project-code/server.py
@@ -19,22 +19,7 @@ app.add_api("master.yml")
 @app.route("/")
 def home():
    return render_template("home.html")
-"""@app.route("/upload", methods=['POST'])
-def upload():
-   target = os.path.join(APP_ROOT, 'input/')
-   print(target)
 
-   if not os.path.isdir(target):
-      os.mkdir(target)
-
-   for file in request.files.getlist("file"):
-      print(file)
-      filename = file.filename
-      destination = "/".join([target, filename])
-      print(destination)
-      file.save(destination)
-
-   return render_template("complete.html")"""
 
 
 if __name__ == "__main__":
